016a9160983e0ee466c9bd8278e307332c4e86b1
masset151 <masset.ruiz151@gmail.com>
2021-06-08 19:12:09 +0200

    Auth

--- public/main.js
+++ public/main.js
@@ -338,8 +338,8 @@ __webpack_require__.r(__webpack_exports__);
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 const environment = {
-    production: false,
-    baseUrl: 'http://localhost:3000/api/'
+    production: true,
+    baseUrl: 'https://devproyecto.herokuapp.com/api/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
