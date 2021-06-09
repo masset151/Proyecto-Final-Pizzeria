d6199e3dde87decdf761a3b508762a1d24ab0f92
masset151 <masset.ruiz151@gmail.com>
2021-06-09 18:22:28 +0200

    CORS

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors({origin:'http://devproyecto.herokuapp.com/'}));
-app.use(cors({origin:'http://http://localhost:4200'}));
+app.use(cors({origin:'https://pizzeriasan-andres.herokuapp.com/'}))
+app.use(cors({origin:'http://localhost:4200'}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 //Routes
 
