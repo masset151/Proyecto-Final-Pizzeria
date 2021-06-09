fb04cd192c0553428662d3de1e3d476499d79562
masset151 <masset.ruiz151@gmail.com>
2021-06-09 18:27:08 +0200

    CORS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors')
+//const cors = require('cors')
 const path = require('path')
 const app = express();
 const {mongoose} = require("./database")
@@ -13,13 +13,7 @@ app.use(express.static('public'));
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+
 
 
 //Routes
